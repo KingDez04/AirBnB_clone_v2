6da9a7fa8db1b979f1f02d2c42070d47dd6aba92
Ademola Ademeso <desmondademeso@gmail.com>
2024-06-27 20:42:20 +0100

    Tweaked test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,75 +1,41 @@
 #!/usr/bin/python3
-"""Defines the HBNB Test console."""
+"""
+Contains the class TestConsoleDocs
+"""
 
+import console
+import inspect
+import pep8
 import unittest
-from unittest.mock import patch
-from io import StringIO
-from console import HBNBCommand
-import os
-from models import storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+HBNBCommand = console.HBNBCommand
 
 
-class TestConsole(unittest.TestCase):
-    """Test the console."""
-    def setUp(self):
-        """Set up the test."""
-        self.console = HBNBCommand()
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def tearDown(self):
-        """Clean up."""
-        self.console = None
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_create(self):
-        """Test the create command."""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create BaseModel")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create User")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create State")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create City")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create Amenity")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create Place")
-            self.assertTrue(f.getvalue() != "")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("create Review")
-            self.assertTrue(f.getvalue() != "")
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
-    def test_show(self):
-        """Test the show command."""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show BaseModel")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show User")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show State")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show City")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show Amenity")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show Place")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd("show Review")
-            self.assertTrue(f.getvalue() == "** class doesn't exist **\n")
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
