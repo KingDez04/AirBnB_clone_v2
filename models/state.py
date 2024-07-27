43afa5bb10037fdb528846ea8e0cdd57f828684b
Ademola Ademeso <desmondademeso@gmail.com>
2024-07-28 00:07:20 +0100

    Default state.py

--- models/state.py
+++ models/state.py
@@ -2,7 +2,6 @@
 """Defines the State class."""
 import models
 from os import getenv
-from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.base_model import BaseModel
 from models.city import City
@@ -10,7 +9,6 @@ import sqlalchemy
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-import shlex
 
 
 class State(BaseModel, Base):
@@ -26,7 +24,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City",  backref="state", cascade="delete, delete-orphan, all")
+        cities = relationship("City",  backref="state", cascade="delete")
     else:
         name = ""
 
@@ -44,14 +42,7 @@ class State(BaseModel, Base):
         def cities(self):
             """Get a list of all related City objects."""
             city_list = []
-            var = models.storage.all()
-            lista = []
-            for key in var:
-                city = key.replace('.', ' ')
-                city = shlex.split(city)
-                if (city[0] == 'City'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.state_id == self.id):
-                    city_list.append(elem)
-            return (city_list)
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
